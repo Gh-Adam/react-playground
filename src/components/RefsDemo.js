e1c33866c089614b956b9ee8457010799b20b2f9
Gh-Adam <ghada.adam.2011@gmail.com>
2021-01-12 23:38:41 +0200

    Updaing RefsDemo

--- src/components/RefsDemo.js
+++ src/components/RefsDemo.js
@@ -5,18 +5,40 @@ class RefsDemo extends Component {
     super(props);
 
     this.inputRef = React.createRef();
+
+    this.inputCallbackRef = null;
+    this.setInputCallbackRef = (element) => (this.inputCallbackRef = element);
   }
 
   componentDidMount() {
-    console.log('input ref', this.inputRef)
-    this.inputRef.current.focus()
+    // focus on input one Mounting
+    console.log('input ref', this.inputRef);
+    // this.inputRef.current.focus();
+
+    // callbackRef case
+    console.log('input callback ref', this.inputCallbackRef);
+    this.inputCallbackRef.focus();
   }
+
+  clickHandler = () => {
+    alert(this.inputRef.current.value);
+  };
 
   render() {
     return (
-      <div>
-        <input type="text" ref={this.inputRef} />
-      </div>
+      <>
+        <div className="text-center my-5">
+          <input type="text" ref={this.inputRef} />
+          <input type="text" ref={this.setInputCallbackRef} />
+          <button
+            type="button"
+            className="btn btn-primary mx-2"
+            onClick={this.clickHandler}
+          >
+            click
+          </button>
+        </div>
+      </>
     );
   }
 }
